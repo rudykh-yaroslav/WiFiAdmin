9b916efc450fa208f8d3bfa64401a9cf9c874b32
Рудых Ярослав Игоревич <Rudykh.Y.Ig@sberbank.ru>
2019-10-25 00:27:57 +0300

    authorization + login in report

--- src/main/kotlin/com/honeygoose/wifiadmin/service/UserService.kt
+++ src/main/kotlin/com/honeygoose/wifiadmin/service/UserService.kt
@@ -2,20 +2,24 @@ package com.honeygoose.wifiadmin.service
 
 import com.honeygoose.wifiadmin.exception.ServiceException
 import com.honeygoose.wifiadmin.model.User
+import com.honeygoose.wifiadmin.model.client.UserData
 import com.honeygoose.wifiadmin.repo.UserRepository
 import org.springframework.dao.DataAccessException
 import org.springframework.stereotype.Service
-import java.util.*
+import java.util.UUID
+import javax.transaction.Transactional
 
 @Service
 class UserService(var userRepository: UserRepository) {
-    fun login(login: String, password: String): String {
+
+    @Transactional
+    fun login(login: String, password: String): UserData {
         try {
             val user = userRepository.findByLoginAndPassword(login, password)
             val token = UUID.randomUUID().toString()
             user.token = token
             userRepository.save(user)
-            return token // user dto
+            return UserData(login = login, token = token) // user dto
         } catch (e: DataAccessException) {
             throw ServiceException() // e
         }
