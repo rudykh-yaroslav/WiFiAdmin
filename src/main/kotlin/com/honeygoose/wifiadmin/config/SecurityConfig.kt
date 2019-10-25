1c220374834a993a7864793c87e957df5b161aa6
Рудых Ярослав Игоревич <Rudykh.Y.Ig@sberbank.ru>
2019-10-25 05:23:26 +0300

    fixed

--- src/main/kotlin/com/honeygoose/wifiadmin/config/SecurityConfig.kt
+++ src/main/kotlin/com/honeygoose/wifiadmin/config/SecurityConfig.kt
@@ -4,6 +4,7 @@ import com.honeygoose.wifiadmin.model.UserRole
 import com.honeygoose.wifiadmin.security.AuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
@@ -12,12 +13,11 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 import org.springframework.security.web.util.matcher.OrRequestMatcher
-import org.springframework.http.HttpStatus
-import org.springframework.security.web.authentication.HttpStatusEntryPoint
-import org.springframework.security.web.AuthenticationEntryPoint
 
 @Configuration
 @EnableWebSecurity
@@ -29,7 +29,7 @@ class SecurityConfig(
             AntPathRequestMatcher("/api/v1/admin/**"))
 
     override fun configure(auth: AuthenticationManagerBuilder?) {
-        auth?.authenticationProvider(provider);
+        auth?.authenticationProvider(provider)
     }
 
     override fun configure(web: WebSecurity?) {
@@ -66,6 +66,6 @@ class SecurityConfig(
 
     @Bean
     fun forbiddenEntryPoint(): AuthenticationEntryPoint {
-        return HttpStatusEntryPoint(HttpStatus.FORBIDDEN)
+        return HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
     }
 }
