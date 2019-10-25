002623bca53f51b21742b9faf67db7e5ebd95e20
Рудых Ярослав Игоревич <Rudykh.Y.Ig@sberbank.ru>
2019-10-25 03:13:49 +0300

    fixed

--- src/main/kotlin/com/honeygoose/wifiadmin/model/client/WiFiData.kt
+++ src/main/kotlin/com/honeygoose/wifiadmin/model/client/WiFiData.kt
@@ -1,10 +1,6 @@
 package com.honeygoose.wifiadmin.model.client
 
 data class WiFiData(
-        val login: String,
         val wiFiDetails: List<WiFiDetail>? = null,
-        val wiFiConnection: WiFiConnection? = null,
-        val lat: Double? = null,
-        val lon: Double? = null,
-        val address: String? = null
+        val wiFiConnection: WiFiConnection? = null
 )
